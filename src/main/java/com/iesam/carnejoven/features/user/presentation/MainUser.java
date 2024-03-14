16d1db317bd32f7a87945a8e2fab2cb6bd007492
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2024-03-14 14:50:53 +0100

    .

--- src/main/java/com/iesam/carnejoven/features/user/presentation/MainUser.java
+++ src/main/java/com/iesam/carnejoven/features/user/presentation/MainUser.java
@@ -2,6 +2,7 @@ package com.iesam.carnejoven.features.user.presentation;
 
 import com.iesam.carnejoven.features.user.data.UserDataRepository;
 import com.iesam.carnejoven.features.user.data.local.UserFileDataSource;
+import com.iesam.carnejoven.features.user.domain.GetUserUseCase;
 import com.iesam.carnejoven.features.user.domain.SaveUserUseCase;
 import com.iesam.carnejoven.features.user.domain.User;
 
@@ -17,6 +18,7 @@ public class MainUser {
     public static void getUser(String nif){
         UserFileDataSource userFileDataSource = UserFileDataSource.newInstance();
         UserDataRepository userDataRepository = new UserDataRepository(userFileDataSource);
+        GetUserUseCase getUserUseCase = new GetUserUseCase(userDataRepository);
         User user = userDataRepository.getUser(nif);
         System.out.println(user.toString());
     }
